7d6cc6be1a9369290925addb85969533972d679d
David Doty <doty@ucdavis.edu>
2023-01-29 15:39:23 -0800

    Update crn.py

--- gpac/crn.py
+++ gpac/crn.py
@@ -25,7 +25,7 @@ we can write
     gpac.plot_crn(rxns, init_config, t_eval)
 
 which will plot the concentrations of A, B, and U over time. One can specify reversible reactions
-by using the `|` operator instead of ``>>`` (e.g., ``a+b | 2*u``) and rate constants using the functions
+by using the ``|`` operator instead of ``>>`` (e.g., ``a+b | 2*u``) and rate constants using the functions
 ``k`` (for forward rate constants) and ``r`` (for reverse rate constants),
 e.g., ``(a+b | 2*u).k(1.5).r(0.5)``.
 
