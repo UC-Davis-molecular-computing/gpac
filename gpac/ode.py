ff7de2c24cafb6ec58c70cfdbec991acc1b884aa
Dave Doty <doty@ucdavis.edu>
2023-01-30 10:41:40 -0800

    Update ode.py

--- gpac/ode.py
+++ gpac/ode.py
@@ -304,7 +304,7 @@ def plot(
     Numerically integrate the given ODEs using the function :func:`integrate_odes`,
     then plot the trajectories using matplotlib.
     (Assumes it is being run in a Jupyter notebook.)
-    See :func:`integrate_odes` for description of arguments other than `figure_size` and `symbols_to_plot`.
+    See :func:`integrate_odes` for description of parameters other than `figure_size` and `symbols_to_plot`.
 
     Args:
         figure_size:
